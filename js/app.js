d3220de49d65afbb6e251bdbcb1fbc04648368cb
Kiril Paskov <kiko0o7@abv.bg>
2016-09-26 23:48:55 +0300

    Extraxted: data in app.js

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@ import { pageLoader } from 'page-controller';
 import { eventLoader } from 'event-controller';
 
 let app = new Sammy(function() {
+    let data = {};
 
     this.get(appUrls.MAIN_URL, function() {
         template.get('front-navigation')
@@ -27,13 +28,11 @@ let app = new Sammy(function() {
             .then(() => eventLoader.loadUserNavigationEvents());
 
         let head = header.getHeader(true, false);
-        let data = {};
         request.get(`https://baas.kinvey.com/appdata/${kinveyUrls.KINVEY_APP_ID}/books`, head)
-            .then((res) => { data.books = res })
+            .then((books) => { data.books = books })
             .then(() => {
-                console.log(data.books);
                 request.get(`https://baas.kinvey.com/appdata/${kinveyUrls.KINVEY_APP_ID}/authors`, head)
-                    .then((res) => { data.authors = res })
+                    .then((auth) => { data.authors = auth })
                     .then(() => {
                         template.get('home-page')
                             .then(temp => pageLoader.loadUserHomePage(temp, data))
@@ -45,15 +44,10 @@ let app = new Sammy(function() {
 
     this.get(appUrls.AUTHORS_URL, function() {
         let head = header.getHeader(true, false);
-        let data = {};
 
-        request.get(`https://baas.kinvey.com/appdata/${kinveyUrls.KINVEY_APP_ID}/authors`, head)
-            .then((auth) => { data.authors = auth })
-            .then(() => {
-                template.get('authors-page')
-                    .then(temp => pageLoader.loadAuthorsPage(temp, data))
-                    .then(() => eventLoader.loadAuthorsPageEvents())
-            });
+        template.get('authors-page')
+            .then(temp => pageLoader.loadAuthorsPage(temp, data))
+            .then(() => eventLoader.loadAuthorsPageEvents());
 
     });
 
